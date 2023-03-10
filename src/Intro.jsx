9547d62701494440ecc0d3564578bfeb82e27203
KrystianMaciejewski <89975505+KrystianMaciejewski@users.noreply.github.com>
2023-03-10 14:48:44 +0100

    app rewritten for better logic and cleaner code

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 
 export default function Intro(props) {
-    
-    
+
+
     return (
         <div>
-        <h1>Quizzical</h1>
-        <h3>Some description if needed</h3>
-        <button onClick={props.startQuiz}>Start quiz</button>
+            <h1>Quizzical</h1>
+            <h3>Some description if needed</h3>
+            <button onClick={props.startQuiz}>Start quiz</button>
         </div>
     )
 }
